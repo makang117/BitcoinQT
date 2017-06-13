2ce7d948aeb41f24b9e160c1171517322604c07a
MK <makang117@gmail.com>
2017-06-13 17:27:14 +0800

    增加conf目录下类文件

---
+++ src/conf/TradeConfigurator.py
@@ -0,0 +1,28 @@
+"""
+Author: Ma Kang
+EMail: mk117@163.com
+CreateDate: 2017-6-13
+"""
+
+
+# 引入包
+import argparse
+import json
+import sys
+import os
+import codecs
+
+
+class TradeConfig:
+
+    #  类私有变量
+    _confdir = "conf"
+    _conffile = "Tradeapi.conf"
+
+    def __init__(self):
+        pass
+
+
+
+
+
